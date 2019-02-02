dd71a17cf99e79bf927fb757d8b46afb89a18509
WIINnikolaev <https://github.com/WINikolaev>
2019-02-02 16:17:16 +0300

    Revert "feat(IWDG): add WatchDog"
    This reverts commit a8a8d32ae590e8b97d8c9b21677cc254368f70e2.

--- TaskVR/Src/main.cpp
+++ TaskVR/Src/main.cpp
@@ -49,9 +49,15 @@ void initAllPerpherals(void)
   MX_TIM4_Init();
   MX_TIM5_Init();
   /* USER CODE BEGIN 2 */
+<<<<<<< HEAD
 
   /* USER CODE END 2 */
 
+=======
+
+  /* USER CODE END 2 */
+
+>>>>>>> parent of a8a8d32... feat(IWDG): add WatchDog
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
@@ -63,6 +69,9 @@ void initAllPerpherals(void)
 }
 
 
+<<<<<<< HEAD
+>>>>>>> parent of a8a8d32... feat(IWDG): add WatchDog
+=======
 >>>>>>> parent of a8a8d32... feat(IWDG): add WatchDog
 void SystemClock_Config(void)
 {
@@ -78,6 +87,9 @@ void SystemClock_Config(void)
   /**Initializes the CPU, AHB and APB busses clocks
   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+<<<<<<< HEAD
+>>>>>>> parent of a8a8d32... feat(IWDG): add WatchDog
+=======
 >>>>>>> parent of a8a8d32... feat(IWDG): add WatchDog
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
